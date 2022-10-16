49b826e1c42039da6c4c52829589ab99cac2ab5b
Nika <nikarasevas@gmail.com>
2022-10-16 16:07:14 +0300

    Рекурсия: 1) строка с числами от a до b: a <= b 2) сумма чисел от 1 до n

---
+++ Exercise_01_02/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 1: Собрать строку с числами от a до b, a <= b */
+
+// string NumbersRec(int a, int b)
+// {
+//     if(a <= b)
+//     {
+//         return ($"{a} " + NumbersRec(a + 1, b));
+//     }
+//     else
+//     {
+//         return String.Empty;
+//     }
+// }
+// Console.WriteLine(NumbersRec(1, 10));
+
+/* Задача 2: Сумма чисел от 1 до n */
+
+int SumRec(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else 
+    {
+        return(n + SumRec(n - 1));
+    }
+}
+Console.WriteLine(SumRec(10));
